f9f38f409575eac455ff2c52af0a84cff78d777d
Ashhar <ashhar.shah786@gmail.com>
2022-11-11 14:31:55 +0500

    Finalized Prediction Script

--- script/script.py
+++ script/script.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv('dataset/used_car_data_cleaned.csv')
 
